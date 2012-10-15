1485d24716d47b4774b48c277a788102f81b2024
Qiming Sun <osirpt.sun@gmail.com>
2012-10-15 18:13:51 -0400

    Finish coul_mat, untested

--- src/coulumb/CouLatSum.cc
+++ src/coulumb/CouLatSum.cc
@@ -29,6 +29,7 @@ static struct maybe_env_t {
     FBasisSet *p_bas;
     FOpMatrix *p_den_mat;
     phf::coulomb::grid_3d *p_grid3d;
+    double grid_weight;
 };
 
 static struct maybe_env_t _env;
@@ -189,6 +190,8 @@ static int is_cell_far(const int cell_id[3])
     //}
 }
 
+// for a given grid_id in 0-th cell,
+// iterate this fn in cells_accumulator to run over all the image grids
 static void density_cell0(const int grid_id[3], const int cell_id[3],
                           double *v, double *dmat, double *ao)
 {
@@ -204,35 +207,23 @@ static void density_cell0(const int grid_id[3], const int cell_id[3],
     coord[2] = grid_coord[2] - cell_coord[2];
     ao_super_cell(coord, ao);
 
-    int n_mu = _env.p_den_mat->nCols;
-    int n_nu = _env.p_den_mat->nRows;
+    int n_mu = _env.p_den_mat->nRows;
+    int n_nu = _env.p_den_mat->nCols;
     *v += dtrace_vmv(n_mu, n_nu, ao, ao, dmat);
 }
 // constrain grid_id in 0-th cell
 static double density_at_grid(const int grid_id[3],
                               double *v, double *dmat, double *ao)
 {
-
     double den;
     cells_accumulator(density_at_grid_iter, is_cell_far,
                       grid_id, &den, _env.p_den_mat, ao);
     return den - _env.background_chg;
 }
 
+// for a given grid_id in 0-th cell,
+// iterate this fn in cells_accumulator to run over all the image grids
 static int coul_matrix_cell_iter(const int grid_id[3], const int cell_id[3],
-                                 double *v, double *mat, double *ao0)
-{
-    int num_ao = _env.nao_unit_cell;
-    double *ao_mu = new double[num_ao];
-    double *ao_nu = new double[num_ao];
-    double den;
-    grids_accumulator(coul_matrix_grid_iter, v, mat, ao);
-    delete[] ao_mu;
-    delete[] ao_nu;
-    return den;
-}
-
-static int coul_matrix_grid_iter(const int grid_id[3], cell_id,
                                  double *v, double *dmat, double *ao)
 {
     double grid_coord[3];
@@ -240,31 +231,32 @@ static int coul_matrix_grid_iter(const int grid_id[3], cell_id,
     double cell_coord[3];
     cell_coord_by_cell_id(cell_id, cell_grid);
     double coord[3];
-    coord[0] = grid_coord[0] - cell_coord[0];
-    coord[1] = grid_coord[1] - cell_coord[1];
-    coord[2] = grid_coord[2] - cell_coord[2];
+    coord[0] = grid_coord[0] + cell_coord[0];
+    coord[1] = grid_coord[1] + cell_coord[1];
+    coord[2] = grid_coord[2] + cell_coord[2];
     ao_super_cell(coord, ao);
 
-    int n_mu = _env.p_den_mat->nCols;
-    int n_nu = _env.p_den_mat->nRows;
-    *v += dtrace_vmv(n_mu, n_nu, ao, ao, dmat);
-
-
-
+    int n_mu = _env.p_den_mat->nRows;
+    int n_nu = _env.p_den_mat->nCols;
     const int INC1 = 1;
-    const double D0 = 0;
-    const double D1 = 1;
-    const char TRANS_T = 'T';
-    dger_(&num_ao, &num_ao, v, mu, &INC1, nu, &INC1, dmat, ao_mu);
+    double vw = *v * _env.grid_weight;
+    dger_(&n_mu, &n_nu, &vw, ao, &INC1, ao, &INC1, dmat, n_mu);
 }
 
-static int coul_matrix(double *v, double *mat)
+static void coul_matrix_grid_iter(const int grid_id[3],
+                                  double *v, double *dmat, double *ao)
+{
+    // for any grids in 0-th cell, accumulate the contribution of image grids
+    cells_accumulator(coul_matrix_cell_iter, is_cell_far,
+                      grid_id, v, dmat, ao);
+}
+
+static void coul_matrix_acc(double *real_space_pot, double *vmat)
 {
     double *ao = new double[_env.p_den_mat->nCols];
-    ao_unit_cell(grid_id, cell_id, ao_mu);
-    cells_accumulator(coul_matrix_cell_iter, is_cell_far,
-                      grid_id, &den, dmat, ao0);
-    return 0;
+    // accumulate all the contributions of the grids in 0-th cell
+    grids_accumulator(coul_matrix_grid_iter, real_space_pot, vmat, ao);
+    delete[] ao;
 }
 
 
@@ -273,7 +265,7 @@ static int coul_matrix(double *v, double *mat)
  **************************************************
  * global functions
  */
-int init_coul_fft(FSolidModel& solid, FOpMatrix& den_mat)
+int init_coul_fft(const FSolidModel& solid, const FOpMatrix& den_mat)
 {
     _env.p_solid = &solid;
     _env.p_super_cell = &solid.SuperCell;
@@ -290,6 +282,7 @@ int init_coul_fft(FSolidModel& solid, FOpMatrix& den_mat)
     set_background_charge(nele/vol);
 
     _env.p_grid3d = new phf::coulomb::grid_3d();
+    _env.grid_weight = vol / (grid_3d.num_x * grid_3d.num_y * grid_3d.num_z)
 }
 
 int del_coul_fft()
@@ -327,18 +320,29 @@ void density_unit_cell(double *density)
 }
 
 // return the coulomb energy
-double coul_matrix(FSolidModel& solid, FOpMatrix& dm, FOpMatrix& p_coul_mat)
+double coul_matrix(const FSolidModel& solid, const FOpMatrix& den_mat,
+                   FOpMatrix& coul_mat)
 {
     init_coul_fft(solid, dm);
+
+    int num_grid_in_unit_cell = _env.p_grid_3d->num_x
+                              * _env.p_grid_3d->num_y
+                              * _env.p_grid_3d->num_z;
+    double *real_space_density = new double[num_grid_in_unit_cell];
+    double *real_space_pot = new double[num_grid_in_unit_cell];
     // calculate the density
-    density_unit_cell();
+    density_unit_cell(real_space_density);
     // FFT Poisson solver
-    poisson_solver();
+    poisson_solver(real_space_density, real_space_pot);
     // Coulomb matrix
-    coul_matrix();
+    coul_matrix_acc(real_space_pot, &coul_mat[0]);
+    delete[] real_space_density;
+    delete[] real_space_pot;
+
     del_coul_fft();
-    return dot_prod(p_coul_mat, dm);
-;
+    const int INC1 = 1;
+    int n = coul_mat.size();
+    return ddot_(&n, &coul_mat[0], &INC1, &den_mat[0], &INC1);
 }
 
 } // end namespace cls
